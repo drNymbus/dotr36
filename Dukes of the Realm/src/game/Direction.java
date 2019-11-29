cba040a55e179bf1238a41fe3446720cf8df9bb0
vmelancon <victor.melancon@etu.u-bordeaux.fr>
2019-11-29 11:59:50 +0100

    1er commit

---
+++ Dukes of the Realm/src/game/Direction.java
@@ -0,0 +1,8 @@
+package game;
+
+enum Direction {
+    NORTH,
+    SOUTH,
+    EAST,
+    WEST;
+}
---
+++ Dukes of the Realm/src/game/Sprite.java
@@ -0,0 +1,37 @@
+package game;
+
+import javafx.scene.shape.Rectangle;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+
+public class Sprite {
+    private Pane layer;
+    private int x, y;
+    private Rectangle rect;
+
+    public Sprite(Pane layer, Color c, int x, int y, int size) {
+        this.rect = new Rectangle(x, y, size, size);
+        this.rect.setFill(c);
+        this.layer = layer;
+        addToLayer();
+    }
+
+    public void addToLayer() { this.layer.getChildren().add(rect); }
+
+    public void removeFromLayer() { this.layer.getChildren().remove(rect); }
+
+    public void setX(int x) { this.x = x; this.rect.setX(x); }
+    public int getX() { return this.x; }
+    public void setY(int x) { this.y = x; this.rect.setY(x); }
+    public int getY() { return this.y; }
+
+    public Rectangle getShape() { return this.rect; }
+    public int getSize() { return this.rect.getWidth(); }
+
+    public boolean isIn(int x, int y) {
+        if (x >= this.x && x <= this.x + this.getSize()) {
+            if (y >= this.y && y <= this.y + this.getSize()) return true;
+        }
+        return false;
+    }
+}
