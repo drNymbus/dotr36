dcd9a9814ae7f479d05c1d9f38dba5cf1564bff6
maxime <maxime.denis33@gmail.com>
2020-01-04 18:07:53 +0100

    ajout d'une IA, modification de sprite x et y, c'est le centre du rectangle et non le coin. ajustement de isIn en conséquence. Les chevaux sont bourrés.

---
+++ Dukes of the Realm/src/game/EnnemyIA.java
@@ -0,0 +1,40 @@
+package game;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class EnnemyIA {
+
+	Battlefield bf;
+	Random rand = new Random();
+	public EnnemyIA(Battlefield b) {
+		this.bf = b;
+	}
+
+	public void update_Ennemy() {
+		ArrayList<Castle> c = new ArrayList<Castle>();
+		ArrayList<Castle> ce = new ArrayList<Castle>();
+
+		for (Castle castle : bf.getCastles()) {
+			if (castle.getOwner() == -1) {
+				c.add(castle);
+			} else
+				ce.add(castle);
+		}
+		int nb = rand.nextInt(c.size());
+		int move = rand.nextInt(101);
+		if (move < 30) {
+			c.get(nb).addProd(TypeSoldier.Piquier);
+			System.out.println("piquier");
+		} else if (move > 29 && move < 60) {
+			c.get(nb).addProd(TypeSoldier.Chevalier);
+			System.out.println("chevalier");
+		} else if (move > 59 && move < 90) {
+			c.get(nb).addProd(TypeSoldier.Onagre);
+			System.out.println("onagre");
+		} else if (move > 89) {
+			c.get(nb).setTarget(ce.get(rand.nextInt(ce.size())).getId());
+			System.out.println("attack");
+		}
+	}
+}
